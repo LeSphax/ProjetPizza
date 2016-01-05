a92d18bcfbf1d5173e5211ec21d6c16d7040513a
Sébastien Kerbrat <Sbastien Kerbrat>
2016-01-05 11:44:52 +0100

    Changed TableLayout to Flow Layout, Started creating Command panel

--- PrototypeGSI/Form1.Designer.vb
+++ PrototypeGSI/Form1.Designer.vb
@@ -22,24 +22,26 @@ Partial Class Form1
     'Ne la modifiez pas à l'aide de l'éditeur de code.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Dim TreeNode1 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Les Meilleures")
-        Dim TreeNode2 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Végétariennes")
-        Dim TreeNode3 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Hallal")
-        Dim TreeNode4 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Type", New System.Windows.Forms.TreeNode() {TreeNode1, TreeNode2, TreeNode3})
-        Dim TreeNode5 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Tomate")
-        Dim TreeNode6 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Crème fraiche")
-        Dim TreeNode7 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Harissa")
-        Dim TreeNode8 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Sauce", New System.Windows.Forms.TreeNode() {TreeNode5, TreeNode6, TreeNode7})
+        Dim TreeNode17 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Les Meilleures")
+        Dim TreeNode18 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Végétariennes")
+        Dim TreeNode19 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Hallal")
+        Dim TreeNode20 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Type", New System.Windows.Forms.TreeNode() {TreeNode17, TreeNode18, TreeNode19})
+        Dim TreeNode21 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Tomate")
+        Dim TreeNode22 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Crème fraiche")
+        Dim TreeNode23 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Harissa")
+        Dim TreeNode24 As System.Windows.Forms.TreeNode = New System.Windows.Forms.TreeNode("Sauce", New System.Windows.Forms.TreeNode() {TreeNode21, TreeNode22, TreeNode23})
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Panel1 = New System.Windows.Forms.Panel()
         Me.TreeView1 = New System.Windows.Forms.TreeView()
         Me.Panel5 = New System.Windows.Forms.Panel()
         Me.Panel4 = New System.Windows.Forms.Panel()
-        Me.FilterPanel = New System.Windows.Forms.TableLayoutPanel()
-        Me.PizzaLayoutPanel = New System.Windows.Forms.TableLayoutPanel()
         Me.Panel2 = New System.Windows.Forms.Panel()
         Me.Panel3 = New System.Windows.Forms.Panel()
         Me.Panel6 = New System.Windows.Forms.Panel()
+        Me.CommandeTitleLabel = New System.Windows.Forms.Label()
+        Me.FilterPanel = New System.Windows.Forms.FlowLayoutPanel()
+        Me.PizzaLayoutPanel = New System.Windows.Forms.FlowLayoutPanel()
+        Me.Panel5.SuspendLayout()
         Me.Panel4.SuspendLayout()
         Me.Panel2.SuspendLayout()
         Me.Panel3.SuspendLayout()
@@ -48,11 +50,10 @@ Partial Class Form1
         '
         'Label1
         '
-        Me.Label1.Anchor = System.Windows.Forms.AnchorStyles.Top
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(71, 3)
+        Me.Label1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Label1.Location = New System.Drawing.Point(0, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(49, 17)
+        Me.Label1.Size = New System.Drawing.Size(200, 40)
         Me.Label1.TabIndex = 1
         Me.Label1.Text = "Pizzas"
         Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
@@ -72,23 +73,23 @@ Partial Class Form1
         Me.TreeView1.Dock = System.Windows.Forms.DockStyle.Fill
         Me.TreeView1.Location = New System.Drawing.Point(0, 40)
         Me.TreeView1.Name = "TreeView1"
-        TreeNode1.Name = "Type1"
-        TreeNode1.Text = "Les Meilleures"
-        TreeNode2.Name = "Type2"
-        TreeNode2.Text = "Végétariennes"
-        TreeNode3.Name = "Type3"
-        TreeNode3.Text = "Hallal"
-        TreeNode4.Name = "Type"
-        TreeNode4.Text = "Type"
-        TreeNode5.Name = "Sauce1"
-        TreeNode5.Text = "Tomate"
-        TreeNode6.Name = "Sauce2"
-        TreeNode6.Text = "Crème fraiche"
-        TreeNode7.Name = "Sauce3"
-        TreeNode7.Text = "Harissa"
-        TreeNode8.Name = "Sauce"
-        TreeNode8.Text = "Sauce"
-        Me.TreeView1.Nodes.AddRange(New System.Windows.Forms.TreeNode() {TreeNode4, TreeNode8})
+        TreeNode17.Name = "Type1"
+        TreeNode17.Text = "Les Meilleures"
+        TreeNode18.Name = "Type2"
+        TreeNode18.Text = "Végétariennes"
+        TreeNode19.Name = "Type3"
+        TreeNode19.Text = "Hallal"
+        TreeNode20.Name = "Type"
+        TreeNode20.Text = "Type"
+        TreeNode21.Name = "Sauce1"
+        TreeNode21.Text = "Tomate"
+        TreeNode22.Name = "Sauce2"
+        TreeNode22.Text = "Crème fraiche"
+        TreeNode23.Name = "Sauce3"
+        TreeNode23.Text = "Harissa"
+        TreeNode24.Name = "Sauce"
+        TreeNode24.Text = "Sauce"
+        Me.TreeView1.Nodes.AddRange(New System.Windows.Forms.TreeNode() {TreeNode20, TreeNode24})
         Me.TreeView1.Size = New System.Drawing.Size(200, 851)
         Me.TreeView1.TabIndex = 5
         '
@@ -96,10 +97,11 @@ Partial Class Form1
         '
         Me.Panel5.BackColor = System.Drawing.SystemColors.Control
         Me.Panel5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel5.Controls.Add(Me.CommandeTitleLabel)
         Me.Panel5.Dock = System.Windows.Forms.DockStyle.Right
-        Me.Panel5.Location = New System.Drawing.Point(864, 0)
+        Me.Panel5.Location = New System.Drawing.Point(864, 100)
         Me.Panel5.Name = "Panel5"
-        Me.Panel5.Size = New System.Drawing.Size(200, 891)
+        Me.Panel5.Size = New System.Drawing.Size(200, 791)
         Me.Panel5.TabIndex = 5
         '
         'Panel4
@@ -109,43 +111,8 @@ Partial Class Form1
         Me.Panel4.Dock = System.Windows.Forms.DockStyle.Top
         Me.Panel4.Location = New System.Drawing.Point(0, 0)
         Me.Panel4.Name = "Panel4"
-        Me.Panel4.Size = New System.Drawing.Size(864, 100)
+        Me.Panel4.Size = New System.Drawing.Size(1064, 100)
         Me.Panel4.TabIndex = 4
-        '
-        'FilterPanel
-        '
-        Me.FilterPanel.AutoScroll = True
-        Me.FilterPanel.ColumnCount = 3
-        Me.FilterPanel.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle())
-        Me.FilterPanel.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle())
-        Me.FilterPanel.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle())
-        Me.FilterPanel.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.FilterPanel.Location = New System.Drawing.Point(0, 0)
-        Me.FilterPanel.Name = "FilterPanel"
-        Me.FilterPanel.RowCount = 2
-        Me.FilterPanel.RowStyles.Add(New System.Windows.Forms.RowStyle())
-        Me.FilterPanel.RowStyles.Add(New System.Windows.Forms.RowStyle())
-        Me.FilterPanel.Size = New System.Drawing.Size(862, 98)
-        Me.FilterPanel.TabIndex = 0
-        '
-        'PizzaLayoutPanel
-        '
-        Me.PizzaLayoutPanel.AutoScroll = True
-        Me.PizzaLayoutPanel.ColumnCount = 4
-        Me.PizzaLayoutPanel.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle())
-        Me.PizzaLayoutPanel.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle())
-        Me.PizzaLayoutPanel.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle())
-        Me.PizzaLayoutPanel.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle())
-        Me.PizzaLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.PizzaLayoutPanel.Location = New System.Drawing.Point(0, 100)
-        Me.PizzaLayoutPanel.Name = "PizzaLayoutPanel"
-        Me.PizzaLayoutPanel.RowCount = 4
-        Me.PizzaLayoutPanel.RowStyles.Add(New System.Windows.Forms.RowStyle())
-        Me.PizzaLayoutPanel.RowStyles.Add(New System.Windows.Forms.RowStyle())
-        Me.PizzaLayoutPanel.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20.0!))
-        Me.PizzaLayoutPanel.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20.0!))
-        Me.PizzaLayoutPanel.Size = New System.Drawing.Size(864, 791)
-        Me.PizzaLayoutPanel.TabIndex = 6
         '
         'Panel2
         '
@@ -169,13 +136,41 @@ Partial Class Form1
         'Panel6
         '
         Me.Panel6.Controls.Add(Me.PizzaLayoutPanel)
-        Me.Panel6.Controls.Add(Me.Panel4)
         Me.Panel6.Controls.Add(Me.Panel5)
+        Me.Panel6.Controls.Add(Me.Panel4)
         Me.Panel6.Dock = System.Windows.Forms.DockStyle.Fill
         Me.Panel6.Location = New System.Drawing.Point(200, 67)
         Me.Panel6.Name = "Panel6"
         Me.Panel6.Size = New System.Drawing.Size(1064, 891)
         Me.Panel6.TabIndex = 7
+        '
+        'CommandeTitleLabel
+        '
+        Me.CommandeTitleLabel.Dock = System.Windows.Forms.DockStyle.Top
+        Me.CommandeTitleLabel.Location = New System.Drawing.Point(0, 0)
+        Me.CommandeTitleLabel.Name = "CommandeTitleLabel"
+        Me.CommandeTitleLabel.Size = New System.Drawing.Size(198, 33)
+        Me.CommandeTitleLabel.TabIndex = 0
+        Me.CommandeTitleLabel.Text = "Ma commande"
+        Me.CommandeTitleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'FilterPanel
+        '
+        Me.FilterPanel.AutoScroll = True
+        Me.FilterPanel.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.FilterPanel.Location = New System.Drawing.Point(0, 0)
+        Me.FilterPanel.Name = "FilterPanel"
+        Me.FilterPanel.Size = New System.Drawing.Size(1062, 98)
+        Me.FilterPanel.TabIndex = 0
+        '
+        'PizzaLayoutPanel
+        '
+        Me.PizzaLayoutPanel.AutoScroll = True
+        Me.PizzaLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.PizzaLayoutPanel.Location = New System.Drawing.Point(0, 100)
+        Me.PizzaLayoutPanel.Name = "PizzaLayoutPanel"
+        Me.PizzaLayoutPanel.Size = New System.Drawing.Size(864, 791)
+        Me.PizzaLayoutPanel.TabIndex = 0
         '
         'Form1
         '
@@ -187,10 +182,10 @@ Partial Class Form1
         Me.Controls.Add(Me.Panel1)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.Panel5.ResumeLayout(False)
         Me.Panel4.ResumeLayout(False)
         Me.Panel2.ResumeLayout(False)
         Me.Panel3.ResumeLayout(False)
-        Me.Panel3.PerformLayout()
         Me.Panel6.ResumeLayout(False)
         Me.ResumeLayout(False)
 
@@ -199,10 +194,11 @@ Partial Class Form1
     Friend WithEvents Panel5 As Panel
     Friend WithEvents TreeView1 As TreeView
     Friend WithEvents Panel4 As Panel
-    Friend WithEvents PizzaLayoutPanel As TableLayoutPanel
     Friend WithEvents Panel1 As Panel
     Friend WithEvents Panel2 As Panel
     Friend WithEvents Panel3 As Panel
     Friend WithEvents Panel6 As Panel
-    Friend WithEvents FilterPanel As TableLayoutPanel
+    Friend WithEvents CommandeTitleLabel As Label
+    Friend WithEvents FilterPanel As FlowLayoutPanel
+    Friend WithEvents PizzaLayoutPanel As FlowLayoutPanel
 End Class
